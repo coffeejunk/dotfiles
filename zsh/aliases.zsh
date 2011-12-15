47ca02f357d43329dd1555f619020f7b92323951
Maximilian Haack <mxhaack@gmail.com>
2011-12-15 19:39:42 +0100

    alias gti to git

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,7 @@ alias ll='ls -l'
 alias pg_start="pg_ctl -D /usr/local/var/postgres start > /dev/null"
 alias pg_stop="pg_ctl -D /usr/local/var/postgres stop > /dev/null"
 
+alias gti='git'
 alias gst='git status'
 alias gp='git push'
 alias gb='git branch'
