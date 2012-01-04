e3fec9883426e2908f6bef9bdeffeb0fe396659a
Maximilian Haack <mxhaack@gmail.com>
2012-01-04 16:41:54 +0100

    add function to change git email

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,8 @@ alias gti='git'
 alias gst='git status'
 alias gp='git push'
 alias gb='git branch'
+
+change_git_mail() {
+  ruby -e "fp = File.expand_path('~/.gitconfig'); f = File.read(fp); File.open(fp, 'w') {|file| file.puts f.gsub!(/email\s*=.*/, 'email = $1')}"
+}
+alias home="change_git_mail 'mxhaack@gmail.com'"
